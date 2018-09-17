0458f77a04d0a0515d04fa401e04c5eb416dc325
lijiaocn <lijiaocn@foxmail.com>
2018-09-17 11:36:02 +0800

    m

---
+++ assets/js/head.js
@@ -0,0 +1,19 @@
+/*
+ * head.js
+ * Copyright (C) 2018 lijiaocn <lijiaocn@foxmail.com>
+ *
+ * Distributed under terms of the GPL license.
+ */
+/*baidu auto push*/
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
